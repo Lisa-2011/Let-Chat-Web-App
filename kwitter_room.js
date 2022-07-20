a7430f7fab152b2be28b42e784dfda0a43aed791
Lisa-2011 <93234101+Lisa-2011@users.noreply.github.com>
2022-07-20 19:47:09 -0400

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -52,3 +52,10 @@ function redirectToRoomName(name){
     localStorage.setItem("room_name" , name);
     window.location = "kwitter_page.html"
 }
+
+function logout(){
+    localStorage.removeItem("room_name");
+    localStorage.removeItem("user_name");
+    window.location = "kwitter.html";
+
+}
